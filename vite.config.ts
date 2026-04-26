6cc19f5a7461b480f076a540ffedc8a34788748e
Raihan Syeka Pramukastie <rehaansekap@gmail.com>
2026-04-26 11:52:51 +0700

    Github Action Test Fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"],
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
         }),
         inertia(),
