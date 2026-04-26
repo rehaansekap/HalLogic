d6d198eb7885abfe8e98a27efaacbd5fa059601c
Raihan Syeka Pramukastie <rehaansekap@gmail.com>
2026-04-26 11:58:05 +0700

    fix syntax error in Vite directive in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,7 +39,7 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
     @viteReactRefresh
-    @vite(['resources/css/app.css', 'resources/js/app.tsx')
+    @vite(['resources/css/app.css', 'resources/js/app.tsx'])
     <x-inertia::head>
         <title>{{ config('app.name', 'Laravel') }}</title>
     </x-inertia::head>
